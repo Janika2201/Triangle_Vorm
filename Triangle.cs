8212ce514ed95ef8d44f0555e264c2bb169d974c
JanikaValjataga <janikavaljataga@gmail.com>
2020-05-19 15:09:37 +0300

    Add project files

---
+++ Triangle.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EO.Internal;
+
+namespace Triangle_Vorm
+{
+    class Triangle
+    {
+        public double a;
+        public double b;
+        public double c;
+    }
+}
+public Triangle(double A, double B, double C)
+{
+    a = A;
+    b = B;
+    с = С;
+}
