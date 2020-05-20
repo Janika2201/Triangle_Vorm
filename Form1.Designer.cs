1216e0e59290cbef8fc36484b4f99749a260b80e
JanikaValjataga <janikavaljataga@gmail.com>
2020-05-20 15:37:41 +0300

    - одно поле, помимо a,b и c.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -30,6 +30,8 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
@@ -39,25 +41,37 @@
             this.button1.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
             this.button1.FlatAppearance.BorderSize = 10;
             this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F);
-            this.button1.Location = new System.Drawing.Point(528, 119);
+            this.button1.Font = new System.Drawing.Font("Mongolian Baiti", 27.75F);
+            this.button1.Location = new System.Drawing.Point(548, 231);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(184, 82);
+            this.button1.Size = new System.Drawing.Size(203, 85);
             this.button1.TabIndex = 0;
             this.button1.Text = "Запуск";
             this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(271, 183);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(784, 361);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.button1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
             this.Text = "Работа с треугольником";
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -65,6 +79,7 @@
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
--- Triangle.cs
+++ Triangle.cs
@@ -9,14 +9,93 @@ namespace Triangle_Vorm
 {
     class Triangle
     {
-        public double a;
-        public double b;
-        public double c;
+        public double a; // первая сторона
+        public double b; // вторая сторона
+        public double c; // третья сторона
+        public double h;
+        public Triangle(double A, double B, double C, double H)//конструктор 
+        {
+            a = A;
+            b = B;
+            c = C;
+            h = H;//высота
+        }
+        public string outputA() // выводим сторону а, данный метод возвращает строковое значение
+        {
+            return Convert.ToString(a); // a - ссылка на внутренее поле обьекта класса
+        }
+        public string outputB()
+        {
+            return Convert.ToString(b);
+        }
+        public string outputC()
+        {
+            return Convert.ToString(c);
+        }
+        public double AreaOfTriangle() // нахождение площади
+        {
+            double S = 0;
+            S = 1 / 2 * b * h;
+            return S;
+        }
+        public double Perimeter() // сумма всех сторон типо double
+        {
+            double p = 0;
+            p = a + b + c; // вычисление
+            return p; // возврат
+        }
+        public double Surface() // аналогично периметру
+        {
+            double h = 0;
+            double p = 0;
+            p = (a + b + c) / 2;
+            h = Math.Sqrt((p * (p - a) * (p - b) * (p - c)));
+            return h;
+        }
+        public double Area()//вычисление площади 
+        {
+            double S = 0;
+            S = (a * h) / 2;
+            return S;
+        }
+        public double GetSetA // свойство позволяющее установить либо изменить значение стороны а
+        {
+            get //устанавливаем
+            { return a; }
+            set // меняем
+            { a = value; }
+        }
+        public double GetSetB
+        {
+            get
+            { return b; }
+            set
+            { b = value; }
+        }
+        public double GetSetC
+        {
+            get
+            { return c; }
+            set
+            { c = value; }
+        }
+        public double GetSetH//свойство высоты
+        {
+            get
+            { return h; }
+            set
+            {
+                h = value;
+            }
+        }
+        public bool ExistTriangle // свойство позволяющее установить, существует ои треугольник с задаными сторонами
+        {
+            get
+            {
+                if ((a > b + c) && (b > a + c) && (c > a + b)) //сумма 2 сторон должна быть больше третьей
+                    return false;
+                else return true;
+            }
+        }
     }
-}
-public Triangle(double A, double B, double C)
-{
-    a = A;
-    b = B;
-    с = С;
 }
