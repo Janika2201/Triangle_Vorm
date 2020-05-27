2ccdfd7fe42fbe3be18487ae59a4ef0ff9cbfec2
Janika2201 <62281674+Janika2201@users.noreply.github.com>
2020-05-27 20:12:38 +0300

    2 часть

--- Form1.cs
+++ Form1.cs
@@ -21,21 +21,20 @@ namespace Triangle_Vorm
         {
             double a, b, c;
             a = Convert.ToDouble(txtA.Text); // считываем значение стороны а
-            b = Convert.ToDouble(txtB.Text);
-            c = Convert.ToDouble(txtC.Text);
-            Triangle triangle = new Triangle(a, b, c);
-            listView1.Items.Add("Сторона а");
-            listView1.Items.Add("Сторона b");
-            listView1.Items.Add("Сторона c");
-            listView1.Items.Add("Высота  h");
-            listView1.Items.Add("Существует?");
-            listView1.Items.Add("Спецификатор");
-            listView1.Items[0].SubItems.Add(triangle.outputA());
-            listView1.Items[1].SubItems.Add(triangle.outputB());
-            listView1.Items[2].SubItems.Add(triangle.outputC());
-            listView1.Items[3].SubItems.Add(Convert.ToString(triangle.Perimeter()));
-            listView1.Items[4].SubItems.Add(Convert.ToString(triangle.Surface()));
-            if (triangle.ExistTriangle) { listView1.Items[5].SubItems.Add("Существует"); }
+            b = Convert.ToDouble(txtB.Text);// считываем значение стороны b
+            c = Convert.ToDouble(txtC.Text);// считываем значение стороны c
+            Triangle triangle = new Triangle(a, b, c);// создаем объект класса Triangle с именем triangle
+            listView1.Items.Add("Сторона а");// добавляем соответсвующие ячейки в коллекцию items объекта listview1
+            listView1.Items.Add("Сторона b");//
+            listView1.Items.Add("Сторона c");//
+            listView1.Items.Add("Существует?");//
+            listView1.Items.Add("Спецификатор");//
+            listView1.Items[0].SubItems.Add(triangle.outputA());//
+            listView1.Items[1].SubItems.Add(triangle.outputB());//
+            listView1.Items[2].SubItems.Add(triangle.outputC());//
+            listView1.Items[3].SubItems.Add(Convert.ToString(triangle.Perimeter()));//выводим периметр
+            listView1.Items[4].SubItems.Add(Convert.ToString(triangle.Surface())); // выводим значение площади
+            if (triangle.ExistTriangle) { listView1.Items[5].SubItems.Add("Существует"); }// свойство Triangle.exist
             else listView1.Items[6].SubItems.Add("Не существует");
 
 
